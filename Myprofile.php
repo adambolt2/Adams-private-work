59ea117f6542821c2ba51b33f4069ba8afdfc694
adambolt2 <120136691+adambolt2@users.noreply.github.com>
2023-10-23 11:16:23 +0100

    Update Myprofile.php

--- Myprofile.php
+++ Myprofile.php
@@ -1357,7 +1357,7 @@ $encodedHomeToken = urlencode($tok);
 ?>
 <html lang="en">
 <head>
-<title> Buddy ChippedIn</title>
+<title> SampleText</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!--<meta http-equiv=""Expires" CONTENT="0">-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
